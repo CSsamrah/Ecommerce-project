231e0025912d3077f8e614a16f8da2feb8cc5ec9
HasanCT282 <hasanarsalan781@gmail.com>
2025-03-05 12:32:53 +0500

    Database connection

---
+++ server/dbConnect.js
@@ -0,0 +1,28 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: './.env'
+}); 
+
+const pool = new Pool({
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    ssl: {
+        rejectUnauthorized: false,     
+    },
+});
+
+pool.connect()
+  .then(() => console.log('Connected to supabase!'))
+  .catch(err => {
+    console.error('Database connection error:', err.message);
+
+});
+
+
+export default pool;
