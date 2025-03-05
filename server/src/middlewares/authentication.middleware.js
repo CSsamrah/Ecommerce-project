7f1f3ea6715e89f6234d58d2f008a9495ff16ee4
Ali Arslan <hasanarsalan781@gmail.com>
2025-03-05 17:24:28 +0500

    authentication

---
+++ server/src/middlewares/authentication.middleware.js
@@ -0,0 +1,28 @@
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import pool from "../../dbConnect.js";
+
+const isLoggedIn=asyncHandler(async(req,res,next)=>{
+    const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "");
+    console.log("TOKEN: ",token);
+
+    if(!token){
+        throw new ApiError(401,'Unauthorized request: No token received');
+    }
+    const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+    console.log("\nDecoded Token: ",decodedToken);
+
+    if(!decodedToken){
+        throw new ApiError(401,'Unauthorized request: Invalid token');
+    }
+    const findUser=await pool.query('SELECT * FROM "Users" WHERE user_id=$1',[decodedToken.id]);
+    if(!findUser.rows.length){
+        throw new ApiError(401,'Invalid Access Token');
+    }
+    req.user = findUser.rows[0];
+    next(); 
+})
+
+export { isLoggedIn }
