d4e9e79e9268c477b28aa64a33f4f36f37d8e5cb
CSSamrah <samra.fatima.790@gmail.com>
2025-03-06 12:44:21 +0500

    changes in main

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./src/routes/userRoutes.js";
 
 dotenv.config();
 
@@ -10,9 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Backend is running!");
-});
+app.use("/",userRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
