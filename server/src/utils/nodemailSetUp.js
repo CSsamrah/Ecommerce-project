e5edad25cc0b13f23b354478b1644560f41dd926
Ali Arslan <arsalanli873@gmail.com>
2025-03-08 00:00:17 +0500

    Fixed

--- server/src/utils/nodemailSetUp.js
+++ server/src/utils/nodemailSetUp.js
@@ -2,8 +2,8 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
     host: process.env.NODEMAIL_HOST,
-    port: 587,
-    secure: false, // true for port 465, false for other ports
+    port: Number(process.env.NODEMAIL_PORT),
+    secure: false, 
     auth: {
       user: process.env.NODEMAIL_USER,
       pass: process.env.NODEMAIL_PASS,
